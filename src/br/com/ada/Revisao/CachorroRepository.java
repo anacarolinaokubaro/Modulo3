7da4aa601c40aa3f95653a337af414c8d12552c9
ana.okubaro <anacarolinaokubaro@gmail.com>
2022-11-04 22:13:24 -0300

    Array dinamico

--- src/br/com/ada/Revisao/CachorroRepository.java
+++ src/br/com/ada/Revisao/CachorroRepository.java
@@ -4,31 +4,34 @@ import java.util.Objects;
 
 public class CachorroRepository {
 
-    private Cachorro[] cadastro = new Cachorro[10];
+    private static int qtd = Cachorro.getId();
+    private static Cachorro[] cadastro = new Cachorro[qtd];
 
-    public void cadastrar(Cachorro cachorro) {
+    public static void cadastrar(Cachorro cachorro) {
         if (cachorro.getDono() == null) {
             System.out.println("Existe um cachorro sem dono, não iremos incluir a lista.");
         } else {
             for (int i = 0; i < cadastro.length; i++) {
-                if (Objects.isNull(cadastro[i])) {
-                    cadastro[i] = cachorro;
-                    break;
-                }
+                if (Objects.isNull(cadastro[i])){
+                cadastro[i] = cachorro;
+                break;
             }
         }
-    }
+    }}
 
-    public void mostraCadastroCachorros(){
+
+    public void mostraCadastroCachorros() {
         System.out.println("-----------------CADASTRO DOGS--------------------");
-        for (Cachorro cachorro: cadastro) {
+        for (Cachorro cachorro : cadastro) {
             if (Objects.nonNull(cachorro)) {
                 System.out.println(cachorro);
             }
         }
     }
 
-
-
-
 }
+
+
+
+
+
